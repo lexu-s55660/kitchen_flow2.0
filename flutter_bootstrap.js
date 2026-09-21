dcf10fe5dd7af57b37040d4dc3e4961e9230dd87
lexu-s55660 <rudometov.alexei@gmail.com>
2026-09-21 17:27:48 +0400

    Чистый деплой на kitchen_flow2.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1978451581" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2175600294" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
